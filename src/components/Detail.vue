a78734c4e0627bea010e56861fb725ab2543521e
liuzhipeng <liuzhipengdongjing@gmail.com>
2023-11-10 08:17:55 +0900

    改elementplus封装的样式，文字居中

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,28 +1,29 @@
 <template>
-    <el-table :data="tableData" border style="width: 100%">
-        <el-table-column prop="date" label="日付" width="180" />
-        <el-table-column prop="week" label="曜日" width="180" />
-        <el-table-column prop="startDate" label="開始" width="110">
-            <div style="display: flex;">
-                <el-input v-model="input" placeholder="時" />
-                <el-input style="margin-left: 1px;" v-model="input" placeholder="分" />
-            </div>
-        </el-table-column>
-        <el-table-column prop="endDate" label="終了" width="110">
-            <div style="display: flex;">
-                <el-input v-model="input" placeholder="時" />
-                <el-input style="margin-left: 1px;" v-model="input" placeholder="分" />
-            </div>
-        </el-table-column>
-        <el-table-column prop="lunchBreak" label="昼休み(時間)" width="180" />
-        <el-table-column prop="workTime" label="勤務時間" width="180" />
-        <el-table-column prop="others" label="備考" width="180" />
-    </el-table>
+    <div class="tbl">
+        <el-table :data="tableData" border style="width: 100%">
+            <el-table-column prop="date" label="日付" width="180" text-align="center" />
+            <el-table-column prop="week" label="曜日" width="180" />
+            <el-table-column prop="startDate" label="開始" width="110">
+                <div style="display: flex;">
+                    <el-input v-model="input" placeholder="時" />
+                    <el-input style="margin-left: 1px;" v-model="input" placeholder="分" />
+                </div>
+            </el-table-column>
+            <el-table-column prop="endDate" label="終了" width="110">
+                <div style="display: flex;">
+                    <el-input v-model="input" placeholder="時" />
+                    <el-input style="margin-left: 1px;" v-model="input" placeholder="分" />
+                </div>
+            </el-table-column>
+            <el-table-column prop="lunchBreak" label="昼休み(時間)" width="180" />
+            <el-table-column prop="workTime" label="勤務時間" width="180" />
+            <el-table-column prop="others" label="備考" width="180" />
+        </el-table>
+    </div>
 </template>
   
 <script lang="ts" setup>
 import { ref } from 'vue';
-
 const input = ref('')
 const tableData = [
     {
@@ -44,5 +45,12 @@ const tableData = [
         others: ''
     },
 ]
+
 </script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.tbl {
+    ::v-deep(.el-table .cell) {
+        text-align: center;
+    }
+}
+</style>
