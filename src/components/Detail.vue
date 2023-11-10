5a8d9a4bc08fef0e2ea980c7351f32ed58b54b06
liuzhipeng <liuzhipengdongjing@gmail.com>
2023-11-10 14:00:44 +0900

    レイアウト1.0完了

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,29 +1,34 @@
 <template>
     <div class="tbl">
-        <el-table :data="tableData" border style="width: 100%">
-            <el-table-column prop="date" label="日付" width="180" text-align="center" />
-            <el-table-column prop="week" label="曜日" width="180" />
-            <el-table-column prop="startDate" label="開始" width="110">
-                <div style="display: flex;">
-                    <el-input v-model="input" placeholder="時" />
-                    <el-input style="margin-left: 1px;" v-model="input" placeholder="分" />
-                </div>
+        <el-table :data="tableData" border style="width: 100%" :row-style="rowState" :header-cell-style="headerRowState">
+            <el-table-column prop="date" label="日付" width="110" />
+            <el-table-column prop="week" label="曜日" width="70" />
+            <el-table-column prop="startDate" label="開始時刻" width="130">
+                <el-time-select v-model="input" start="09:00" step="00:15" end="23:59" placeholder="00:00" />
             </el-table-column>
-            <el-table-column prop="endDate" label="終了" width="110">
-                <div style="display: flex;">
-                    <el-input v-model="input" placeholder="時" />
-                    <el-input style="margin-left: 1px;" v-model="input" placeholder="分" />
-                </div>
+            <el-table-column prop="endDate" label="終了時刻" width="130">
+                <el-time-select v-model="input" start="09:00" step="00:15" end="23:59" placeholder="00:00" />
             </el-table-column>
-            <el-table-column prop="lunchBreak" label="昼休み(時間)" width="180" />
-            <el-table-column prop="workTime" label="勤務時間" width="180" />
-            <el-table-column prop="others" label="備考" width="180" />
+            <el-table-column prop="lunchBreak" label="昼休(時間)" width="100" />
+            <el-table-column prop="workTime" label="勤務時間" width="100" />
+            <el-table-column prop="others" label="備考" width="180">
+                <el-input v-model="input" :rows="1" type="textarea" />
+            </el-table-column>
+            <el-table-column prop="others" label="操作" width="60">
+                <el-button :icon="Edit" circle />
+            </el-table-column>
         </el-table>
     </div>
 </template>
   
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import { Edit } from '@element-plus/icons-vue'
+
+onMounted(() => {
+
+})
+
 const input = ref('')
 const tableData = [
     {
@@ -46,11 +51,36 @@ const tableData = [
     },
 ]
 
+// 行内css
+const rowState = ({ row, rowIndex }: { row: string, rowIndex: string }) => {
+    if (rowIndex == '1') {
+        return {
+            // backgroundColor: 'red'
+        }
+    }
+}
+
+// ヘッダcss
+const headerRowState = ({ row, rowIndex }: { row: string, rowIndex: string }) => {
+    if (rowIndex == '0') {
+        return {
+            // backgroundColor: '#E8E9E9 !important'
+        };
+    }
+};
+
 </script>
 <style scoped lang="scss">
 .tbl {
     ::v-deep(.el-table .cell) {
         text-align: center;
+    }
+
+    ::v-deep(.el-table) {
+        border-collapse: collapse;
+        border-spacing: 0;
+        border-radius: 20px;
+        border: solid 10px rgb(74, 74, 74);
     }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
+import 'element-plus/theme-chalk/dark/css-vars.css'
 const app = createApp(App)
 
 app.use(ElementPlus)
