f26e2f7ad01d0727be88b1f9714c65c91d86e103
liuzhipeng <liuzhipengdongjing@gmail.com>
2023-11-17 14:42:23 +0900

    fix

--- src/hooks/detailHook/useMonth.ts
+++ src/hooks/detailHook/useMonth.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { computed, ref } from "vue"
+import { ref } from "vue"
 import useCalendarDataStore from "../../store/calendarData"
 
 export default function () {
