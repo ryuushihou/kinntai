fc561bc827ea4396b5bd9d63502f35827167cf08
liuzhipeng <liuzhipengdongjing@gmail.com>
2024-10-11 00:10:07 +0900

    commit 3 (+2 squashed commits) Squashed commits: [2988e22] commit 2 [56018f5] commit 1

--- src/App.vue
+++ src/App.vue
@@ -25,15 +25,15 @@
           <div @click="backToSelectMonth()"
             style="height: 45px; width: 125px; margin-left: 10px; display: flex; align-items: center;"
             class="transition-all group duration-200 ease-in-out hover:border-l-8 hover:ring-2 ring-brand-primary hover:border-brand-primary cursor-pointer relative bg-white block bg-white border rounded-xl p-4 transition-all group duration-200 ease-in-out hover:border-l-8 hover:ring-2 ring-brand-primary hover:border-brand-primary cursor-pointer relative bg-white">
-            <span style="font-weight: 500; font-size: 15px; text-align: center;">月分選択へ</span>
+            <span style="font-weight: 500; font-size: 15px; text-align: center;">月分選択へ1</span>
           </div>
           <div @click="print()" style="height: 45px; width: 125px; margin-left: 10px; display: flex; align-items: center;"
             class="transition-all group duration-200 ease-in-out hover:border-l-8 hover:ring-2 ring-brand-primary hover:border-brand-primary cursor-pointer relative bg-white block bg-white border rounded-xl p-4 transition-all group duration-200 ease-in-out hover:border-l-8 hover:ring-2 ring-brand-primary hover:border-brand-primary cursor-pointer relative bg-white">
-            <span style="font-weight: 500; font-size: 15px; text-align: center; margin: 0 auto;">PDF出力</span>
+            <span style="font-weight: 500; font-size: 15px; text-align: center; margin: 0 auto;">PDF出力1</span>
           </div>
           <div @click="save()" style="height: 45px; width: 125px; margin-left: 10px; display: flex; align-items: center;"
             class="transition-all group duration-200 ease-in-out hover:border-l-8 hover:ring-2 ring-brand-primary hover:border-brand-primary cursor-pointer relative bg-white block bg-white border rounded-xl p-4 transition-all group duration-200 ease-in-out hover:border-l-8 hover:ring-2 ring-brand-primary hover:border-brand-primary cursor-pointer relative bg-white">
-            <span style="font-weight: 500; font-size: 15px; text-align: center; margin: 0 auto;">保存</span>
+            <span style="font-weight: 500; font-size: 15px; text-align: center; margin: 0 auto;">保存1</span>
           </div>
 
         </div>
